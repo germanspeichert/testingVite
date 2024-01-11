cdf742c24fbd623d445ef003a78b55a6467d444e
German Speichert <german.speichert@gmail.com>
2024-01-11 15:08:39 -0300

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(15)
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<p>hi ger</p>}/>  
+        <Route path="/insertion" element={<p>hi ger, insertion</p>}/>  
+        <Route path="/selection" element={<p>hi ger, selection</p>}/>  
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
+
+
+// return (
+//   <>
+//     <div>
+//       <a href="https://vitejs.dev" target="_blank">
+//         <img src={viteLogo} className="logo" alt="Vite logo" />
+//       </a>
+//       <a href="https://react.dev" target="_blank">
+//         <img src={reactLogo} className="logo react" alt="React logo" />
+//       </a>
+//     </div>
+//     <h1>Vite + React</h1>
+//     <div className="card">
+//       <button onClick={() => setCount((count) => count + 1)}>
+//         count is {count}
+//       </button>
+//       <p>
+//         Edit <code>src/App.jsx</code> and save to test HMR
+//       </p>
+//     </div>
+//     <p className="read-the-docs">
+//       Click on the Vite and React logos to learn more
+//     </p>
+//   </>
+// )
